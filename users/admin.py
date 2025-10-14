2556f2a3c50fe5c7385028f80a3814362397a097
antsave <antsave@icloud.com>
2025-10-14 04:07:03 +0000

    user > admin.py

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import User
 
-# Register your models here.
+@admin.register(User)
+class CustomUserAdmin(UserAdmin):
+    fieldsets = UserAdmin.fieldsets + (
+        ("Profile", {"fields": ("display_name",)}),
+    )
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        (None, {"fields": ("display_name",)}),
+    )
