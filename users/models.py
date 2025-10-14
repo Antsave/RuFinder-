af042feca1bc0a4b604ccdb4d734c7136f50e2ce
antsave <antsave@icloud.com>
2025-10-14 03:59:39 +0000

    Chnaged models.py

--- users/models.py
+++ users/models.py
@@ -1,3 +1,6 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+class User(AbstractUser):
+    # add whatever you want later; this is just an example
+    display_name = models.CharField(max_length=150, blank=True)
