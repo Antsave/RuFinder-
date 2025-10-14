d6f0a7b5c59a5f694bcc8c8a89fedb5d2c81ae92
antsave <antsave@icloud.com>
2025-10-14 04:31:11 +0000

    Serializers

--- posts/serializers.py
+++ posts/serializers.py
@@ -1,7 +1,10 @@
 from rest_framework import serializers
-from .models import Post
+from django.contrib.auth import get_user_model
 
-class PostSerializer(serializers.ModelSerializer):
+User = get_user_model()
+
+class UserSerializer(serializers.ModelSerializer):
     class Meta:
-        model = Post
-        fields = '__all__'
+        model = User
+        fields = ("id", "username", "email", "first_name", "last_name", "display_name")
+        read_only_fields = ("id",)
