674884cfa86152ce8680ef614d3d9f45919ad0b9
antsave <antsave@icloud.com>
2025-10-14 04:44:31 +0000

    changes

--- users/views.py
+++ users/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from django.contrib.auth import get_user_model
+from .serializers import UserSerializer
 
-# Create your views here.
+User = get_user_model()
+
+class MeView(generics.RetrieveUpdateAPIView):
+    serializer_class = UserSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_object(self):
+        return self.request.user
