e82abb9f8349042cfdd3c5d1fc47ad333e95dea7
antsave <antsave@icloud.com>
2025-10-14 04:42:45 +0000

    changes

--- myproject/urls.py
+++ myproject/urls.py
@@ -26,7 +26,7 @@ urlpatterns = [
     path("", home, name="home"),      # <- root path
     path("admin/", admin.site.urls),  
     path("comingsoon/", TemplateView.as_view(template_name="comingsoon.html"), name="comingsoon.html"),
-    path('api/', include('posts.urls')),
     path("api/users/", include("users.urls")),
+    path('api/', include('posts.urls')),
 ]
 
