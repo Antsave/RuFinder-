0a3b3be4afd3f8be916937385bfaa5d481bd82e9
antsave <antsave@icloud.com>
2025-10-14 05:12:47 +0000

    Changed

--- users/views.py
+++ users/views.py
@@ -1,10 +1,12 @@
-from django.contrib.auth import get_user_model
 from rest_framework import generics, permissions
-from .serializers import UserSerializer  # make sure you have this
+from django.contrib.auth import get_user_model
+from .serializers import UserSerializer
 
 User = get_user_model()
 
-class UserListCreateView(generics.ListCreateAPIView):
-    queryset = User.objects.all().order_by("id")
+class MeView(generics.RetrieveUpdateAPIView):
     serializer_class = UserSerializer
-    permission_classes = [permissions.AllowAny]  # or tighten as needed
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_object(self):
+        return self.request.user
