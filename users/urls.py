50745c11ce65225e39704b220373e8c5b1c37441
antsave <antsave@icloud.com>
2025-10-15 00:38:31 +0000

    Added framework import

--- users/urls.py
+++ users/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from .views import MeView, RegisterView
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
     path("me/", MeView.as_view(), name="users-me"),
