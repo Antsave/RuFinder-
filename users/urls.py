706dad0c13585277e9e63a7a8617608c538a5fab
antsave <antsave@icloud.com>
2025-10-15 00:24:30 +0000

    Added to users/urls

--- users/urls.py
+++ users/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import MeView
+from .views import MeView, RegisterView
 
 urlpatterns = [
     path("me/", MeView.as_view(), name="users-me"),
+    path("register/", RegisterView.as_view(), name="users-register"),
 ]
