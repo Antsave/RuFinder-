05fdd4216e075cd4f911605c013532e6903671ae
antsave <antsave@icloud.com>
2025-10-15 00:20:34 +0000

    Added it so a user can fill in fields to create an account

--- users/serializers.py
+++ users/serializers.py
@@ -8,3 +8,18 @@ class UserSerializer(serializers.ModelSerializer):
         model = User
         fields = ("id", "username", "email", "first_name", "last_name", "display_name")
         read_only_fields = ("id",)
+class RegisterSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only =True, min_length=8)
+ 
+    class Meta:
+        model = User
+        # Allow username+email+password at minimum; add more if you want
+        fields = ["id", "username", "email", "password", "first_name", "last_name", "display_name"]
+        read_only_fields = ["id"]
+
+    def create(self, validated_data):
+            password = validated_data.pop("password")
+            user = User(**validated_data)
+            user.set_password(password)      # IMPORTANT: hashes the password
+            user.save()
+            return user
