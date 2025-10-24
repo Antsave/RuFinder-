5a047060377cef3e0c19ad5af467cdd5486415fe
Nikoloz Kurtanidze <nikoloz.ku@gmail.com>
2025-10-24 03:28:33 -0400

    Merged Kpatel0717s Main page with lilianas login and eNK08 profile

---
+++ myproject/views.py
@@ -0,0 +1,22 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.decorators import login_required
+
+def login_view(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect('profile')
+    else:
+        form = AuthenticationForm()
+    return render(request, 'login.html', {'form': form})
+
+@login_required
+def profile_view(request):
+    return render(request, 'profile.html')
